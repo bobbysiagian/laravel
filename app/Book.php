37976bb9ad1219db06a8ec635cf719ffe1931ace
bobbysiagian <bobbysiagian@yahoo.com>
2016-10-25 12:06:45 +0700

    Versi 1.0

---
+++ app/Book.php
@@ -0,0 +1,14 @@
+<?php 
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model {
+   //
+   protected $fillable=[
+        'isbn',
+        'title',
+        'author',
+        'publisher',
+        'image'
+    ];
+}
